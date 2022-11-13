bbacfaf8688b1a03958a32c6d7fb0f4e293f7eb3
Ish Thumber <ishthumber343@gmail.com>
2022-11-13 18:10:33 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Filter").getOrCreate()
-
-df_pyspark = spark.read.csv("test3.csv", header=True, inferSchema=True)
-
-df_pyspark.show()
